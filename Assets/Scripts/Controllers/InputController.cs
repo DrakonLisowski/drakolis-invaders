636b94ebf4401be54d6bbf40fdee094d6e535681
Mika Drakolis <dragon.fox.66@yandex.ru>
2019-04-02 13:27:11 +0300

    Remove useless code. Fix bugged file

--- Assets/Scripts/Controllers/InputController.cs
+++ Assets/Scripts/Controllers/InputController.cs
@@ -1,12 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewMonoBehaviour : MonoBehaviour
+public class InputController : MonoBehaviour
 {
     public GameObject playerShip;
 
+    public float step = 0.5F;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
     private void Update()
     {
+        float axis = -Input.GetAxis("Horizontal");
 
+        playerShip.transform.Translate(new Vector3((float)(axis * step), 0, 0));
+    }
+
+    void Stop()
+    {
+        Cursor.visible = true;
     }
 }
--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -2,23 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public int score;
 
     public int lives;
-
-    public double startSpeed = 1.0;
-
-    public int invadersDead;
-
-    public double boostPerWave = 0.2;
-
-    public int InvadersPerWave => {
-        return 55;
-    };
-
-    public int InvadersSpeed => {
-        return this.startSpeed + (this.invadersDead / this.InvadersPerWave) * this.boostPerWave;
-    };
 }
